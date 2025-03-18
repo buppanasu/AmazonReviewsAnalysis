de0b478a4edb65b7a49401c9aff4ae7c164db460
Alan2201038 <2201038@sit.singaporetech.edu.sg>
2025-03-18 18:56:15 +0800

    Updated Trend Analysis with 2 different outputs.

--- src/main/java/analysis/temporaltrends/TrendDriver.java
+++ src/main/java/analysis/temporaltrends/TrendDriver.java
@@ -10,36 +10,56 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class TrendDriver {
 
 	public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: Trend Driver <input path> <output path>");
+        if (args.length != 3) {
+            System.err.println("Usage: Trend Driver <input path> <output_ALL path> <output_True_Only path>");
             System.exit(-1);
         }
 
         // Set up the Hadoop configuration
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "Review Average Rating and Count");
-        job.setJarByClass(TrendDriver.class);
+        Job jobAllReviews = Job.getInstance(conf, "Review Average Rating and Count for ALL Reviews");
+        jobAllReviews.setJarByClass(TrendDriver.class);
 
         // Set the Mapper and Reducer classes
-        job.setMapperClass(TrendMapper.class);
-        job.setReducerClass(TrendReducer.class);
+        jobAllReviews.setMapperClass(TrendMapper.class);
+        jobAllReviews.setReducerClass(TrendReducer.class);
 
         // Set the output types for the Mapper and Reducer
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
+        jobAllReviews.setOutputKeyClass(Text.class);
+        jobAllReviews.setOutputValueClass(Text.class);
+        
+        Job jobTrueOnlyReviews = Job.getInstance(conf, 
+        		"Review Average Rating and Count for Verified Purchase Reviews");
+        jobTrueOnlyReviews.setJarByClass(TrendDriver.class);
+        
+        jobTrueOnlyReviews.setMapperClass(TrueOnlyTrendMapper.class);
+        jobTrueOnlyReviews.setReducerClass(TrendReducer.class);
+        
+        // Set the output types for the Mapper and Reducer
+        jobTrueOnlyReviews.setOutputKeyClass(Text.class);
+        jobTrueOnlyReviews.setOutputValueClass(Text.class);
 
         // Define Input & Output Paths
         Path inPath = new Path(args[0]);
-        Path outPath = new Path(args[1]);
+        Path outALLPath = new Path(args[1]);
+        Path outTrueOnlyPath = new Path(args[2]);
         
         // Delete Output Path if there is one.
-        outPath.getFileSystem(conf).delete(outPath, true);
+        outALLPath.getFileSystem(conf).delete(outALLPath, true);
+        outTrueOnlyPath.getFileSystem(conf).delete(outTrueOnlyPath, true);
 
-        FileInputFormat.addInputPath(job, inPath);
-        FileOutputFormat.setOutputPath(job, outPath);
+        FileInputFormat.addInputPath(jobAllReviews, inPath);
+        FileOutputFormat.setOutputPath(jobAllReviews, outALLPath);
+        
+        FileInputFormat.addInputPath(jobTrueOnlyReviews, inPath);
+        FileOutputFormat.setOutputPath(jobTrueOnlyReviews, outTrueOnlyPath);
 
-        // Exit the job if it fails
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        // Run both jobs sequentially
+        if (jobAllReviews.waitForCompletion(true)) {
+            System.exit(jobTrueOnlyReviews.waitForCompletion(true) ? 0 : 1);
+        } else {
+            System.exit(1);
+        }
     }
 
 }
